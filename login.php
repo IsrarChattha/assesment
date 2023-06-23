c46df09246a60b1ce6b98ac65031bc9b27ec4fdf
adilwaheed999 <adil.waheed990@gmail.com>
2023-06-23 17:36:34 +0500

    first commit (html form creation and Retrieving form data using $_POST)

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+if (isset($_POST['submit'])) {
+    echo $_POST['username'];
+    echo $_POST['password'];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <form action="login.php" method="POST">
+            <div class="row">
+                <div class="col-md-6">
+                    <label">Username</label>
+                        <input type="text" name="username">
+                </div>
+                <div class="col-md-6">
+                    <label">Password</label>
+                        <input type="password" name="password">
+                </div>
+            </div>
+            <input type="submit" name="submit" value="Submit" class="btn btn-primary">
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+
+</html>
